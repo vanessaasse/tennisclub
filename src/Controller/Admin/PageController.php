fed266dfbdd8801e91692cda0a7dd184f16e4f25
Vanessa Asse <vanessa.asse@gmail.com>
2018-12-14 11:39:08 +0100

    [PHP] Création d'une class PageController pour l'admin

---
+++ src/Controller/Admin/PageController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AdminController as AdminController;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\QueryBuilder;
+use EasyCorp\Bundle\EasyAdminBundle\Event\EasyAdminEvents;
+
+class PageController extends AdminController
+{
+
+
+}
