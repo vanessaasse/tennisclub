ca80fcece50fecc594bda253d624eaa27191f54c
Vanessa Asse <vanessa.asse@gmail.com>
2018-12-16 22:23:44 +0100

    [PHP] Entité User etoffée pour créer la page d'inscription (/register). Création du formulaire UserType

---
+++ src/Form/UserType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('username', TextType::class, array(
+            'label' => "Nom d'utilisateur",
+            'required' => true
+        ))
+            ->add('email', EmailType::class, array(
+                'label' => 'Email',
+                'required' => true
+            ))
+            ->add('plainPassword', RepeatedType::class, array(
+                'type' => PasswordType::class,
+                'first_options'  => array('label' => 'Password'),
+                'second_options' => array('label' => 'Repeat Password')
+            ));
+    }
+
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => User::class
+        ));
+    }
+}
