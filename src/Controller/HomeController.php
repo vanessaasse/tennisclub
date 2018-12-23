ee7754e7a68c04c94d235b3d2b583a7d1f6690a6
Vanessa Asse <vanessa.asse@gmail.com>
2018-12-23 12:25:21 +0100

    [PHP] Ajout des routes vers les pages créées dans le menu dans base.html.twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,19 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Page;
 
 
 class HomeController extends AbstractController
 {
     /**
+     *
      * @Route("/", name="homepage")
+     *
      * @return Response
      */
     public function index():Response
