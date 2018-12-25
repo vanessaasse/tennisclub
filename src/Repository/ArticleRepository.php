86024381529443eb37f159d94917b4ed30f3360c
Vanessa Asse <vanessa.asse@gmail.com>
2018-12-25 17:51:50 +0100

    [PHP] Création de la méthode getPublishedArticles pour afficher uniquement les articles publiés dans la page article/index()

--- src/Repository/ArticleRepository.php
+++ src/Repository/ArticleRepository.php
@@ -19,32 +19,19 @@ class ArticleRepository extends ServiceEntityRepository
         parent::__construct($registry, Article::class);
     }
 
-    // /**
-    //  * @return Article[] Returns an array of Article objects
-    //  */
-    /*
-    public function findByExampleField($value)
+    /**
+     *
+     * @return mixed
+     */
+    public function getPublishedArticles()
     {
-        return $this->createQueryBuilder('a')
-            ->andWhere('a.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('a.id', 'ASC')
-            ->setMaxResults(10)
+        $query = $this->createQueryBuilder('a')
+            ->select('a')
+            ->where('a.isPublished = :isPublished')
+            ->setParameter('isPublished', '1')
             ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
+            ->getResult();
 
-    /*
-    public function findOneBySomeField($value): ?Article
-    {
-        return $this->createQueryBuilder('a')
-            ->andWhere('a.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
+        return $query;
     }
-    */
 }
