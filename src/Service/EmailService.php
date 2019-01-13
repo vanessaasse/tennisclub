cfc4ab8e426edaaa314313540e370ce61cfa048b
Vanessa Asse <vanessa.asse@gmail.com>
2019-01-13 16:28:33 +0100

    [PHP] Création de la méthode sendMailTennisAdult pour gérer l'envoi d'emails via la page Inscription au tennis club

--- src/Service/EmailService.php
+++ src/Service/EmailService.php
@@ -83,4 +83,22 @@ class EmailService
             ->setContentType('text/html');
         $this->mailer->send($message);
     }
+
+
+    /**
+     * @param $data
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
+    public function sendMailTennisAdult($data)
+    {
+        $message = (new \Swift_Message("Inscription au Tennis club"))
+            ->setFrom($data['email'])
+            ->setTo($this->emailFrom)
+            ->setBody($this->templating->render('email/tennisAdult.html.twig',
+                array('data' => $data)))
+            ->setContentType('text/html');
+        $this->mailer->send($message);
+    }
 }
